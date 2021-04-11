c250102b915ba2095d559be556352cd2c9d1faeb
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-04-11 21:34:56 +0530

    Added: Check Length Of Line

--- LineComparison.java
+++ LineComparison.java
@@ -1,6 +1,11 @@
-//Welcome to Line Comparison Program
+
 public class LineComparison {
 
 	public static void main(String[] args) {
+		int x1=0,y1=0;
+		int x2=2,y2=2;
+		double dis=(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+		System.out.println("Length of Line "+dis);
+
 	}
 }
